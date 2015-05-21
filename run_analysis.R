c7b2f33b36786bab35199213d0c3372cabd21557
Hamsterkiller <reven_yas_reven@mail.ru>
2015-05-21 16:17:00 +0300

    row.name option added in run_Analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -16,64 +16,60 @@ if (!require("reshape2")) {
 require("data.table")
 require("reshape2")
 
-# reading labels of the activity
+# Load: activity labels
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
 
-# reading labels of the features,
+# Load: data column names
 features <- read.table("./UCI HAR Dataset/features.txt")[,2]
 
-# selecting only those feature-labels, which match "mean" or "std" patterns
-selected_feature_labels <- grepl("mean|std", features)
+# Extract only the measurements on the mean and standard deviation for each measurement.
+extract_features <- grepl("mean|std", features)
 
 # Load and process X_test & y_test data.
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-# setting names for the X_test vars
+
 names(X_test) = features
+
+# Extract only the measurements on the mean and standard deviation for each measurement.
+X_test = X_test[,extract_features]
+
+# Load activity labels
+y_test[,2] = activity_labels[y_test[,1]]
+names(y_test) = c("Activity_ID", "Activity_Label")
+names(subject_test) = "subject"
+
+# Bind data
+test_data <- cbind(as.data.table(subject_test), y_test, X_test)
 
 # Load and process X_train & y_train data.
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
-# setting names for the X_train vars
 names(X_train) = features
 
-# UNION of the test and train data frames
-X_union <- rbind(X_train, X_test)
-
 # Extract only the measurements on the mean and standard deviation for each measurement.
-X_union = X_union[,selected_feature_labels]
+X_train = X_train[,extract_features]
 
-# UNION of the subject datasets (the same train -> test order)
-subject_union <- rbind(subject_train, subject_test)
+# Load activity data
+y_train[,2] = activity_labels[y_train[,1]]
+names(y_train) = c("Activity_ID", "Activity_Label")
+names(subject_train) = "subject"
 
-# setting name for subject
-subject_union_t <- as.data.table(subject_union)
-setnames(subject_union_t, "subject_num")
+# Bind data
+train_data <- cbind(as.data.table(subject_train), y_train, X_train)
 
-# UNION of the activity-numbers datasets (the same train -> test order)
-y_union <- rbind(y_train, y_test)
+# Merge test and train data
+data = rbind(test_data, train_data)
 
-# adding activity labels
-y_union[, 2] = activity_labels[y_union[, 1]]
+id_labels   = c("subject", "Activity_ID", "Activity_Label")
+data_labels = setdiff(colnames(data), id_labels)
+melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
 
-# setting names to y_union vars
-names(y_union) <- c("activity_num", "activity_label")
+# Apply mean function to dataset using dcast function
+tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
 
-# creating resulting data
-data <- cbind(subject_union_t, y_union, X_union)
-
-# preparing id-vector for melting
-id_labels <- c("subject_num", "activity_num", "activity_label")
-
-# melting data
-meltData <- melt(data, id.vars = id_labels,
-                 measure.vars = setdiff(colnames(data), id_labels))
-
-# Creating tidy data
-tidy_data   = dcast(meltData, subject_num + activity_label ~ variable, mean)
-
-#writing tidy dataset to the file
-write.table(tidy_data, file = "./tidy_data.txt")
+write.table(tidy_data, file = "./tidy_data.txt", row.name=FALSE)
